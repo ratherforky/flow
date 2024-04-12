fa390ff9540109291775cb65d8c82a6a14cf5e5d
ratherforky <jf16688@bristol.ac.uk>
2024-04-13 00:11:18 +0100

    not inlining makes a significant difference, but ghc automatically inlines these operators in most cases. more investigation needed to find an example of the inline heuristics failing

---
+++ source/library/FlowINLINE.hs
@@ -0,0 +1,265 @@
+-- | Flow provides operators for writing more understandable Haskell. It is an
+-- alternative to some common idioms like ('Prelude.$') for function
+-- application and ('Prelude..') for function composition.
+--
+-- Flow is designed to be imported unqualified. It does not export anything
+-- that conflicts with the base package.
+--
+-- >>> import Flow
+--
+-- == Rationale
+--
+-- I think that Haskell can be hard to read. It has two operators for applying
+-- functions. Both are not really necessary and only serve to reduce
+-- parentheses. But they make code hard to read. People who do not already
+-- know Haskell have no chance of guessing what @foo $ bar@ or @baz & qux@
+-- mean.
+--
+-- Those that do know Haskell are forced to read lines forwards and backwards
+-- at the same time, thanks to function composition. Even something simple,
+-- like finding the minimum element, bounces around: @f = head . sort@.
+--
+-- I think we can do better. By using directional operators, we can allow
+-- readers to move their eye in only one direction, be that left-to-right or
+-- right-to-left. And by using idioms common in other programming languages,
+-- we can allow people who aren't familiar with Haskell to guess at the
+-- meaning.
+--
+-- So instead of ('Prelude.$'), I propose ('<|'). It is a pipe, which anyone
+-- who has touched a Unix system should be familiar with. And it points in the
+-- direction it sends arguments along. Similarly, replace ('Prelude.&') with
+-- ('|>'). And for composition, ('<.') replaces ('Prelude..'). I would have
+-- preferred @<<@, but its counterpart @>>@ is taken by Haskell's syntax.
+-- So-called "backwards" composition is normally expressed with
+-- ('Control.Category.>>>'), which Flow provides as ('.>').
+module FlowINLINE
+  ( -- * Function application
+    (|>),
+    (<|),
+    apply,
+
+    -- * Function composition
+    (.>),
+    (<.),
+    compose,
+
+    -- * Strict function application
+    (!>),
+    (<!),
+    apply',
+  )
+where
+
+import Prelude (seq)
+
+-- | Left-associative 'apply' operator. Read as "apply forward" or "pipe into".
+-- Use this to create long chains of computation that suggest which direction
+-- things move in.
+--
+-- >>> 3 |> succ |> recip |> negate
+-- -0.25
+--
+-- Or use it anywhere you would use ('Prelude.&').
+--
+-- prop> \ x -> (x |> f) == f x
+--
+-- prop> \ x -> (x |> f |> g) == g (f x)
+infixl 0 |>
+
+(|>) :: a -> (a -> b) -> b
+x |> f = apply x f
+
+-- | Right-associative 'apply' operator. Read as "apply backward" or "pipe
+-- from". Use this to create long chains of computation that suggest which
+-- direction things move in. You may prefer this operator over ('|>') for
+-- 'Prelude.IO' actions since it puts the last function first.
+--
+-- >>> print <| negate <| recip <| succ <| 3
+-- -0.25
+--
+-- Or use it anywhere you would use ('Prelude.$').
+--
+-- Note that ('<|') and ('|>') have the same precedence, so they cannot be used
+-- together.
+--
+-- >>> -- This doesn't work!
+-- >>> -- print <| 3 |> succ |> recip |> negate
+--
+-- prop> \ x -> (f <| x) == f x
+--
+-- prop> \ x -> (g <| f <| x) == g (f x)
+infixr 0 <|
+
+(<|) :: (a -> b) -> a -> b
+f <| x = apply x f
+
+-- | Function application. This function usually isn't necessary, but it can be
+-- more readable than some alternatives when used with higher-order functions
+-- like 'Prelude.map'.
+--
+-- >>> map (apply 2) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+--
+-- In general you should prefer using an explicit lambda or operator section.
+--
+-- >>> map (\ f -> 2 |> f) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+-- >>> map (2 |>) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+-- >>> map (<| 2) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+--
+-- prop> \ x -> apply x f == f x
+apply :: a -> (a -> b) -> b
+apply x f = f x
+
+-- | Left-associative 'compose' operator. Read as "compose forward" or "and
+-- then". Use this to create long chains of computation that suggest which
+-- direction things move in.
+--
+-- >>> let f = succ .> recip .> negate
+-- >>> f 3
+-- -0.25
+--
+-- Or use it anywhere you would use ('Control.Category.>>>').
+--
+-- prop> \ x -> (f .> g) x == g (f x)
+--
+-- prop> \ x -> (f .> g .> h) x == h (g (f x))
+infixl 9 .>
+
+(.>) :: (a -> b) -> (b -> c) -> (a -> c)
+f .> g = \x -> g (f x) -- compose f g
+{-# INLINE (.>) #-}
+
+-- | Right-associative 'compose' operator. Read as "compose backward" or "but
+-- first". Use this to create long chains of computation that suggest which
+-- direction things move in. You may prefer this operator over ('.>') for
+-- 'Prelude.IO' actions since it puts the last function first.
+--
+-- >>> let f = print <. negate <. recip <. succ
+-- >>> f 3
+-- -0.25
+--
+-- Or use it anywhere you would use ('Prelude..').
+--
+-- Note that ('<.') and ('.>') have the same precedence, so they cannot be used
+-- together.
+--
+-- >>> -- This doesn't work!
+-- >>> -- print <. succ .> recip .> negate
+--
+-- prop> \ x -> (g <. f) x == g (f x)
+--
+-- prop> \ x -> (h <. g <. f) x == h (g (f x))
+infixr 9 <.
+
+(<.) :: (b -> c) -> (a -> b) -> (a -> c)
+g <. f = compose f g
+
+-- | Function composition. This function usually isn't necessary, but it can be
+-- more readable than some alternatives when used with higher-order functions
+-- like 'Prelude.map'.
+--
+-- >>> let fs = map (compose succ) [recip, negate]
+-- >>> map (apply 3) fs
+-- [0.25,-4.0]
+--
+-- In general you should prefer using an explicit lambda or operator section.
+--
+-- >>> map (\ f -> f 3) (map (\ f -> succ .> f) [recip, negate])
+-- [0.25,-4.0]
+-- >>> map (\ f -> f 3) (map (succ .>) [recip, negate])
+-- [0.25,-4.0]
+-- >>> map (\ f -> f 3) (map (<. succ) [recip, negate])
+-- [0.25,-4.0]
+--
+-- prop> \ x -> compose f g x == g (f x)
+
+compose :: (a -> b) -> (b -> c) -> (a -> c)
+compose f g = \x -> g (f x)
+{-# INLINE compose #-}
+
+-- | Left-associative 'apply'' operator. Read as "strict apply forward" or
+-- "strict pipe into". Use this to create long chains of computation that
+-- suggest which direction things move in.
+--
+-- >>> 3 !> succ !> recip !> negate
+-- -0.25
+--
+-- The difference between this and ('|>') is that this evaluates its argument
+-- before passing it to the function.
+--
+-- >>> undefined |> const True
+-- True
+-- >>> undefined !> const True
+-- *** Exception: Prelude.undefined
+-- ...
+--
+-- prop> \ x -> (x !> f) == seq x (f x)
+--
+-- prop> \ x -> (x !> f !> g) == let y = seq x (f x) in seq y (g y)
+infixl 0 !>
+
+(!>) :: a -> (a -> b) -> b
+x !> f = apply' x f
+
+-- | Right-associative 'apply'' operator. Read as "strict apply backward" or
+-- "strict pipe from". Use this to create long chains of computation that
+-- suggest which direction things move in. You may prefer this operator over
+-- ('!>') for 'Prelude.IO' actions since it puts the last function first.
+--
+-- >>> print <! negate <! recip <! succ <! 3
+-- -0.25
+--
+-- The difference between this and ('<|') is that this evaluates its argument
+-- before passing it to the function.
+--
+-- >>> const True <| undefined
+-- True
+-- >>> const True <! undefined
+-- *** Exception: Prelude.undefined
+-- ...
+--
+-- Note that ('<!') and ('!>') have the same precedence, so they cannot be used
+-- together.
+--
+-- >>> -- This doesn't work!
+-- >>> -- print <! 3 !> succ !> recip !> negate
+--
+-- prop> \ x -> (f <! x) == seq x (f x)
+--
+-- prop> \ x -> (g <! f <! x) == let y = seq x (f x) in seq y (g y)
+infixr 0 <!
+
+(<!) :: (a -> b) -> a -> b
+f <! x = apply' x f
+
+-- | Strict function application. This function usually isn't necessary, but it
+-- can be more readable than some alternatives when used with higher-order
+-- functions like 'Prelude.map'.
+--
+-- >>> map (apply' 2) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+--
+-- The different between this and 'apply' is that this evaluates its argument
+-- before passing it to the function.
+--
+-- >>> apply undefined (const True)
+-- True
+-- >>> apply' undefined (const True)
+-- *** Exception: Prelude.undefined
+-- ...
+--
+-- In general you should prefer using an explicit lambda or operator section.
+--
+-- >>> map (\ f -> 2 !> f) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+-- >>> map (2 !>) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+-- >>> map (<! 2) [succ, recip, negate]
+-- [3.0,0.5,-2.0]
+--
+-- prop> \ x -> apply' x f == seq x (f x)
+apply' :: a -> (a -> b) -> b
+apply' x f = seq x (apply x f)
--- source/test-suite/Main.hs
+++ source/test-suite/Main.hs
@@ -2,41 +2,109 @@ import qualified Control.Monad as Monad
 import qualified Flow
 import qualified System.Exit as Exit
 import qualified Test.HUnit as Test
+import qualified Criterion.Main as Cri
+
+import qualified FlowINLINE
+import qualified FlowNOINLINE
+
 
 main :: IO ()
 main = do
-  counts <-
-    Test.runTestTT $
-      Test.TestList
-        [ True Test.~?= True,
-          (3 Flow.|> succ Flow.|> recip Flow.|> negate) Test.~?= (-0.25 :: Double),
-          (negate Flow.<| recip Flow.<| succ Flow.<| 3) Test.~?= (-0.25 :: Double),
-          fmap (Flow.apply 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (2 Flow.|>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (2 Flow.|>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (Flow.<| 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (Flow.apply 3 . Flow.compose succ) [recip, negate]
-            Test.~?= [0.25, -4 :: Double],
-          (succ Flow..> recip Flow..> negate) 3 Test.~?= (-0.25 :: Double),
-          (negate Flow.<. recip Flow.<. succ) 3 Test.~?= (-0.25 :: Double),
-          fmap ((\f -> f 3) . (succ Flow..>)) [recip, negate]
-            Test.~?= [0.25, -4 :: Double],
-          fmap ((\f -> f 3) . (succ Flow..>)) [recip, negate]
-            Test.~?= [0.25, -4 :: Double],
-          fmap ((\f -> f 3) . (Flow.<. succ)) [recip, negate]
-            Test.~?= [0.25, -4 :: Double],
-          (3 Flow.!> succ Flow.!> recip Flow.!> negate) Test.~?= (-0.25 :: Double),
-          (undefined Flow.|> const True) Test.~?= True,
-          (negate Flow.<! recip Flow.<! succ Flow.<! 3) Test.~?= (-0.25 :: Double),
-          (const True Flow.<| undefined) Test.~?= True,
-          fmap (Flow.apply' 2) [succ, recip, negate]
-            Test.~?= [3, 0.5, -2 :: Double],
-          Flow.apply undefined (const True) Test.~?= True,
-          fmap (2 Flow.!>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (2 Flow.!>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
-          fmap (Flow.<! 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double]
-        ]
+  benchmark
+  -- counts <-
+  --   Test.runTestTT $
+  --     Test.TestList
+  --       [ True Test.~?= True,
+  --         (3 Flow.|> succ Flow.|> recip Flow.|> negate) Test.~?= (-0.25 :: Double),
+  --         (negate Flow.<| recip Flow.<| succ Flow.<| 3) Test.~?= (-0.25 :: Double),
+  --         fmap (Flow.apply 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (2 Flow.|>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (2 Flow.|>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (Flow.<| 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (Flow.apply 3 . Flow.compose succ) [recip, negate]
+  --           Test.~?= [0.25, -4 :: Double],
+  --         (succ Flow..> recip Flow..> negate) 3 Test.~?= (-0.25 :: Double),
+  --         (negate Flow.<. recip Flow.<. succ) 3 Test.~?= (-0.25 :: Double),
+  --         fmap ((\f -> f 3) . (succ Flow..>)) [recip, negate]
+  --           Test.~?= [0.25, -4 :: Double],
+  --         fmap ((\f -> f 3) . (succ Flow..>)) [recip, negate]
+  --           Test.~?= [0.25, -4 :: Double],
+  --         fmap ((\f -> f 3) . (Flow.<. succ)) [recip, negate]
+  --           Test.~?= [0.25, -4 :: Double],
+  --         (3 Flow.!> succ Flow.!> recip Flow.!> negate) Test.~?= (-0.25 :: Double),
+  --         (undefined Flow.|> const True) Test.~?= True,
+  --         (negate Flow.<! recip Flow.<! succ Flow.<! 3) Test.~?= (-0.25 :: Double),
+  --         (const True Flow.<| undefined) Test.~?= True,
+  --         fmap (Flow.apply' 2) [succ, recip, negate]
+  --           Test.~?= [3, 0.5, -2 :: Double],
+  --         Flow.apply undefined (const True) Test.~?= True,
+  --         fmap (2 Flow.!>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (2 Flow.!>) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double],
+  --         fmap (Flow.<! 2) [succ, recip, negate] Test.~?= [3, 0.5, -2 :: Double]
+  --       ]
 
-  let hasErrors = Test.errors counts /= 0
-      hasFailures = Test.failures counts /= 0
-  Monad.when (hasErrors || hasFailures) Exit.exitFailure
+  -- let hasErrors = Test.errors counts /= 0
+  --     hasFailures = Test.failures counts /= 0
+  -- Monad.when (hasErrors || hasFailures) Exit.exitFailure
+
+
+
+maps1, maps2, maps3, maps4, maps5 :: [Double] -> [Double]
+maps1 = func
+maps2 = func Flow..> func
+maps3 = func Flow..> func Flow..> func
+maps4 = func Flow..> func Flow..> func Flow..> func
+maps5 = func Flow..> func Flow..> func Flow..> func Flow..> func
+pipeline :: Double -> Double
+pipeline = (+ 1) Flow..> (* 10) Flow..> (/ 2)
+
+func :: [Double] -> [Double]
+func = map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1) Flow..> map (+ 1)
+
+maps1INLINE, maps2INLINE, maps3INLINE, maps4INLINE, maps5INLINE :: [Double] -> [Double]
+maps1INLINE = funcINLINE
+maps2INLINE = funcINLINE FlowINLINE..> funcINLINE
+maps3INLINE = funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE
+maps4INLINE = funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE
+maps5INLINE = funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE FlowINLINE..> funcINLINE
+pipelineINLINE :: Double -> Double
+pipelineINLINE = (+ 1) FlowINLINE..> (* 10) FlowINLINE..> (/ 2)
+
+funcINLINE :: [Double] -> [Double]
+funcINLINE = map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1) FlowINLINE..> map (+ 1)
+
+maps1NOINLINE, maps2NOINLINE, maps3NOINLINE, maps4NOINLINE, maps5NOINLINE :: [Double] -> [Double]
+maps1NOINLINE = map pipelineNOINLINE
+maps2NOINLINE = map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE
+maps3NOINLINE = map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE
+maps4NOINLINE = map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE
+maps5NOINLINE = map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE FlowNOINLINE..> map pipelineNOINLINE
+pipelineNOINLINE :: Double -> Double
+pipelineNOINLINE = (+ 1) FlowNOINLINE..> (* 10) FlowNOINLINE..> (/ 2)
+
+benchmark :: IO ()
+benchmark = Cri.defaultMain
+  [ Cri.bgroup "original"
+      [ Cri.bench "1 map"  $ Cri.nf maps1 [0..1000000]
+      , Cri.bench "2 maps" $ Cri.nf maps2 [0..1000000]
+      , Cri.bench "3 maps" $ Cri.nf maps3 [0..1000000]
+      , Cri.bench "4 maps" $ Cri.nf maps4 [0..1000000]
+      , Cri.bench "5 maps" $ Cri.nf maps5 [0..1000000]
+      ]
+      
+  , Cri.bgroup "INLINE"
+      [ Cri.bench "1 map"  $ Cri.nf maps1INLINE [0..1000000]
+      , Cri.bench "2 maps" $ Cri.nf maps2INLINE [0..1000000]
+      , Cri.bench "3 maps" $ Cri.nf maps3INLINE [0..1000000]
+      , Cri.bench "4 maps" $ Cri.nf maps4INLINE [0..1000000]
+      , Cri.bench "5 maps" $ Cri.nf maps5INLINE [0..1000000]
+      ]
+  , Cri.bgroup "NOINLINE"
+      [ Cri.bench "1 map"  $ Cri.nf maps1NOINLINE [0..1000000]
+      , Cri.bench "2 maps" $ Cri.nf maps2NOINLINE [0..1000000]
+      , Cri.bench "3 maps" $ Cri.nf maps3NOINLINE [0..1000000]
+      , Cri.bench "4 maps" $ Cri.nf maps4NOINLINE [0..1000000]
+      , Cri.bench "5 maps" $ Cri.nf maps5NOINLINE [0..1000000]
+      ]
+
+  ]
